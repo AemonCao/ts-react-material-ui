2f5493871a4650993ad534c025cebe47d1b5741a
Aemon <bot960528@gmail.com>
2020-10-13 17:31:53 +0800

    add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Game from "./components/Tic-Tac-Toe/Game";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.render(
   // 严格模式
   <React.StrictMode>
-    <Game />
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
